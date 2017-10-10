53444b5705a73431d612b5a63b3f352d359bfd8c
Ryan Siu <rsiu@stuy.edu>
2017-10-09 23:02:54 -0400

    Change assignment csv generation

--- generate_assignment_csv.py
+++ generate_assignment_csv.py
@@ -1,8 +1,22 @@
 from app import models, db
 
-output = 'Four Digit,OSIS,Name,Tuesday Signed up,Tuesday Excused,Wednesday Signed Up,Wednesday Excused\n'
+output = 'Four Digit,OSIS,Name,Tuesday Signed up,Tuesday,Wednesday\n'
 
 for u in models.User.query.all():
-    output += str(u.four_digit) + ',' + str(u.osis) + ',' + u.fname + ' ' + u.lname + ',' + str(u.wednesday) + ',' + str(u.wednesday_excused) + ',' + str(u.thursday) + ',' + str(u.thursday_excused) + '\n'
+    if u.wednesday_excused:
+        wed = 'Excused'
+    elif u.wednesday:
+        wed = 'Yes'
+    else:
+        wed = 'No'
+
+    if u.thursday_excused:
+        thurs = 'Excused'
+    elif u.thursday:
+        thurs = 'Yes'
+    else:
+        thurs = 'No'
+    
+    output += str(u.four_digit) + ',' + str(u.osis) + ',' + u.fname.strip() + ' ' + u.lname.strip() + ',' + wed + ',' + thurs + '\n'
 
 open('assignment.csv', 'w').write(output)
